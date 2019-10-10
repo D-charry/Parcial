acb293b2b83b849523340591ced8c2625c348ea1
Diego Charry <>
2019-10-10 10:19:50 -0500

    Creamos los cases para el menu

--- Parcial10-10-19/src/parcial10/pkg10/pkg19/Parcial101019.java
+++ Parcial10-10-19/src/parcial10/pkg10/pkg19/Parcial101019.java
@@ -25,26 +25,27 @@ public class Parcial101019 {
           }
             switch(opcion){
                
-                       case 1:
-                          
-                           break;
-                           
-                       case 2:    
-                           
-                           break;
-                           
-                       case 3:    
-                           break;
-                           
-                       case 4:    
-                           break;
-                           
-                       case 5:
-                          break;
-                           
-                        default:
-                            JOptionPane.showMessageDialog(null,"Su opcion no es valida");
-                        break;
+                case 1:
+                   
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                   
+                default:
+                 
+                    break;
                    }
                    }while (opcion !=8);
     }
