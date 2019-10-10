3fff0b60c81cd3fc458352012ec85f9e881e16e8
Diego Charry <>
2019-10-10 09:49:58 -0500

    Crear el menu

---
+++ Parcial10-10-19/src/parcial10/pkg10/pkg19/Parcial101019.java
@@ -0,0 +1,54 @@
+package parcial10.pkg10.pkg19;
+
+import javax.swing.JOptionPane;
+
+public class Parcial101019 {
+
+    public static void main(String[] args) {
+         int opcion=0;
+       
+        do{
+          try{  
+            
+   opcion = Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la operacion que quiere realizar"
+                    + "\n 1. ¿Que es Commit?"
+                    + "\n 2. ¿Que es un Push?"
+                    + "\n 3. ¿Que es un Pull?"
+                    + "\n 4. ¿Que es una clase?"
+                    + "\n 5. ¿Que es un objeto?"
+                    + "\n 6. Nombre estudiante"
+                    + "\n 7. fecha" 
+                    + "\n 8. Salir"));
+       
+          }catch(Exception ex){
+              JOptionPane.showMessageDialog(null,"Debe ingresar solo valores numericos");
+          }
+            switch(opcion){
+               
+                       case 1:
+                          
+                           break;
+                           
+                       case 2:    
+                           
+                           break;
+                           
+                       case 3:    
+                           break;
+                           
+                       case 4:    
+                           break;
+                           
+                       case 5:
+                          break;
+                           
+                        default:
+                            JOptionPane.showMessageDialog(null,"Su opcion no es valida");
+                        break;
+                   }
+                   }while (opcion !=8);
+    }
+}
+
+    
+
